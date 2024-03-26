0b6249b877d6ac79a5580c480b96cd60fca0e0a0
bm-streamlit <164970663+bm-streamlit@users.noreply.github.com>
2024-03-26 02:52:07 +0000

    Use images

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import sqlite3
 
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
 import altair as alt
 import pandas as pd
 
@@ -181,22 +181,43 @@ st.info('''
     ''')
 
 # Connect to database and create table if needed
-conn, db_was_just_created = connect_db()
+# conn, db_was_just_created = connect_db()
 
-# Initialize data.
-if db_was_just_created:
-    initialize_data(conn)
-    st.toast('Database initialized with some sample data.')
+# # Initialize data.
+# if db_was_just_created:
+#     initialize_data(conn)
+#     st.toast('Database initialized with some sample data.')
 
-# Load data from database
-df = load_data(conn)
+# # Load data from database
+# df = load_data(conn)
+
+df = pd.DataFrame(
+    {"id": [1,2,3], "url": [
+        "https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
+        "https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*",
+        "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"]})
+
+thumbnail_renderer = JsCode("""
+class ThumbnailRenderer {
+init(params) {
+this.eGui = document.createElement('img');
+this.eGui.setAttribute('src', params.value);
+this.eGui.setAttribute('width', 'auto');
+this.eGui.setAttribute('height', 'auto');
+}
+getGui() {
+return this.eGui;
+}
+}
+""")
 
 builder = GridOptionsBuilder.from_dataframe(df)
-builder.configure_default_column(editable=True)
+builder.configure_default_column(editable=True, wrapText=True, autoHeight=True)
 builder.configure_column("id", editable=False)
+builder.configure_column("url", cellRenderer=thumbnail_renderer, flex=True)
 
 # Display data with editable table
-edited_df = AgGrid(df, builder.build())
+edited_df = AgGrid(df, builder.build(), allow_unsafe_jscode=True)
 # st.data_editor(
 #     df,
 #     disabled=['id'], # Don't allow editing the 'id' column.
@@ -208,80 +229,80 @@ edited_df = AgGrid(df, builder.build())
 #     },
 #     key='inventory_table')
 
-has_uncommitted_changes = any(len(v) for v in st.session_state.inventory_table.values())
+# has_uncommitted_changes = any(len(v) for v in st.session_state.inventory_table.values())
 
-st.button(
-    'Commit changes',
-    type='primary',
-    disabled=not has_uncommitted_changes,
-    # Update data in database
-    on_click=update_data,
-    args=(conn, df, st.session_state.inventory_table))
+# st.button(
+#     'Commit changes',
+#     type='primary',
+#     disabled=not has_uncommitted_changes,
+#     # Update data in database
+#     on_click=update_data,
+#     args=(conn, df, st.session_state.inventory_table))
 
 
-# -----------------------------------------------------------------------------
-# Now some cool charts
+# # -----------------------------------------------------------------------------
+# # Now some cool charts
 
-# Add some space
-''
-''
-''
+# # Add some space
+# ''
+# ''
+# ''
 
-st.subheader('Units left', divider='red')
+# st.subheader('Units left', divider='red')
 
-need_to_reorder = df[df['units_left'] < df['reorder_point']].loc[:, 'item_name']
+# need_to_reorder = df[df['units_left'] < df['reorder_point']].loc[:, 'item_name']
 
-if len(need_to_reorder) > 0:
-    items = '\n'.join(f'* {name}' for name in need_to_reorder)
+# if len(need_to_reorder) > 0:
+#     items = '\n'.join(f'* {name}' for name in need_to_reorder)
 
-    st.error(f"We're running dangerously low on the items below:\n {items}")
+#     st.error(f"We're running dangerously low on the items below:\n {items}")
 
-''
-''
+# ''
+# ''
 
-st.altair_chart(
-    # Layer 1: Bar chart.
-    alt.Chart(df)
-        .mark_bar(
-            orient='horizontal',
-        )
-        .encode(
-            x='units_left',
-            y='item_name',
-        )
-    # Layer 2: Chart showing the reorder point.
-    + alt.Chart(df)
-        .mark_point(
-            shape='diamond',
-            filled=True,
-            size=50,
-            color='salmon',
-            opacity=1,
-        )
-        .encode(
-            x='reorder_point',
-            y='item_name',
-        )
-    ,
-    use_container_width=True)
+# st.altair_chart(
+#     # Layer 1: Bar chart.
+#     alt.Chart(df)
+#         .mark_bar(
+#             orient='horizontal',
+#         )
+#         .encode(
+#             x='units_left',
+#             y='item_name',
+#         )
+#     # Layer 2: Chart showing the reorder point.
+#     + alt.Chart(df)
+#         .mark_point(
+#             shape='diamond',
+#             filled=True,
+#             size=50,
+#             color='salmon',
+#             opacity=1,
+#         )
+#         .encode(
+#             x='reorder_point',
+#             y='item_name',
+#         )
+#     ,
+#     use_container_width=True)
 
-st.caption('NOTE: The :diamonds: location shows the reorder point.')
+# st.caption('NOTE: The :diamonds: location shows the reorder point.')
 
-''
-''
-''
+# ''
+# ''
+# ''
 
-# -----------------------------------------------------------------------------
+# # -----------------------------------------------------------------------------
 
-st.subheader('Best sellers', divider='orange')
+# st.subheader('Best sellers', divider='orange')
 
-''
-''
+# ''
+# ''
 
-st.altair_chart(alt.Chart(df)
-    .mark_bar(orient='horizontal')
-    .encode(
-        x='units_sold',
-        y=alt.Y('item_name').sort('-x'),
-    ),
-    use_container_width=True)
+# st.altair_chart(alt.Chart(df)
+#     .mark_bar(orient='horizontal')
+#     .encode(
+#         x='units_sold',
+#         y=alt.Y('item_name').sort('-x'),
+#     ),
+#     use_container_width=True)
