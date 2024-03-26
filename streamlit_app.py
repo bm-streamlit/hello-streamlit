b08173916114df3578bcbf9a3b0ed6f6cb990cbc
bm-streamlit <164970663+bm-streamlit@users.noreply.github.com>
2024-03-26 06:45:44 +0000

    Test

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ from pathlib import Path
 import sqlite3
 
 import streamlit as st
-from st_aggrid import AgGrid, GridOptionsBuilder, JsCode
+from st_aggrid import AgGrid, GridOptionsBuilder, JsCode,GridUpdateMode
 import altair as alt
 import pandas as pd
 
@@ -195,7 +195,8 @@ df = pd.DataFrame(
     {"id": [1,2,3], "url": [
         "https://images.unsplash.com/photo-1608848461950-0fe51dfc41cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxleHBsb3JlLWZlZWR8MXx8fGVufDB8fHx8fA%3D%3D",
         "https://hips.hearstapps.com/hmg-prod/images/cute-cat-photos-1593441022.jpg?crop=0.670xw:1.00xh;0.167xw,0&resize=640:*",
-        "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"]})
+        "https://t4.ftcdn.net/jpg/00/97/58/97/360_F_97589769_t45CqXyzjz0KXwoBZT9PRaWGHRk5hQqQ.jpg"],
+        "label": [False, False, False]})
 
 thumbnail_renderer = JsCode("""
 class ThumbnailRenderer {
@@ -214,10 +215,11 @@ return this.eGui;
 builder = GridOptionsBuilder.from_dataframe(df)
 builder.configure_default_column(editable=True, wrapText=True, autoHeight=True)
 builder.configure_column("id", editable=False)
-builder.configure_column("url", cellRenderer=thumbnail_renderer, flex=True)
+builder.configure_column("url", cellRenderer=thumbnail_renderer, flex=True, editable=False)
 
 # Display data with editable table
-edited_df = AgGrid(df, builder.build(), allow_unsafe_jscode=True, height=200)
+edited_df = AgGrid(df, builder.build(), update_on=[("valueChanged", 5000)], allow_unsafe_jscode=True, height=200)
+print(edited_df["data"])
 # st.data_editor(
 #     df,
 #     disabled=['id'], # Don't allow editing the 'id' column.
